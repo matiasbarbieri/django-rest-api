168f34510f5950661ccf9b4776fdf163cc4c941c
Matias Barbieri <matiasbarbieri@MacBook-Pro-de-Matias.local>
2022-07-23 10:49:05 -0300

    Se agregó serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,16 @@
-import json
 from django.forms.models import model_to_dict
-from products.models import Product
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(["GET"])
+from products.models import Product
+from products.serializers import ProductSerializer
+
+
+@api_view(["POST"])
 def api_home(request, *args, **kwatgs):
-    model_data = Product.objects.all().order_by("?").first()
-    data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id','title','price'])
-    return Response(data)
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        #instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"invalid":"not good data"}, status=400)
