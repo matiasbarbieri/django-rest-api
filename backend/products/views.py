9930802bc87880b342577e6d04723577686c148c
Matias Barbieri <matias.nbarbieri@gmail.com>
2022-07-24 16:24:20 -0300

    Se agrego serializer, create y detail view, dentro de las vistas de los productos

--- backend/products/views.py
+++ backend/products/views.py
@@ -8,6 +8,14 @@ class ProductCreatAPIView(generics.CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+        
+
 product_create_view = ProductCreatAPIView.as_view()
 
 
@@ -16,3 +24,10 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     serializer_class = ProductSerializer
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+product_list_view = ProductListAPIView.as_view()
