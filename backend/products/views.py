f4257f20e0ab8a610a6c07df972fb9f8eb78c890
Matias Barbieri <matiasbarbieri@MacBook-Pro-de-Matias.local>
2022-07-24 13:27:51 -0300

    Se agrego views en el producto

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductCreatAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+product_create_view = ProductCreatAPIView.as_view()
+
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+product_detail_view = ProductDetailAPIView.as_view()
