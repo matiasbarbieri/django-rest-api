912a79bad38bf413770bd5c42e4cf750d89a98d4
Matias Barbieri <matias.nbarbieri@gmail.com>
2022-07-25 13:50:56 -0300

    Mixins and a Generic API View

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,7 @@
 from cgitb import reset
+from requests import request
 
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
@@ -63,8 +64,31 @@ product_destroy_view = ProductDestroyAPIView.as_view()
 
 
 
+class ProductMixinView(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin,generics.GenericAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def get(self, *args, **kwargs):
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = "simple view"
+        serializer.save(content=content)
 
 
+product_mixin_view = ProductMixinView.as_view()
+
+    #def post(self, )
 
 @api_view(['GET', 'POST'])
 def product_alt_view(request, pk=None, *arg, **kwargs):
