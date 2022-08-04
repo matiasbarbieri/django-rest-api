39b54f4c92aac6a4c9bdbaa0e9d90d4f03880f83
Matias Barbieri <matias.nbarbieri@gmail.com>
2022-08-04 19:19:35 -0300

    Second ViewSets and Routers

--- backend/cfehome/routers.py
+++ backend/cfehome/routers.py
@@ -1,10 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
 
-from products.viewsets import ProductViewSet
+from products.viewsets import ProductGenericViewSet
 
 router = DefaultRouter()
-router.register('product-abc', ProductViewSet, basename='products')
-
-print(router.urls)
+router.register('products', ProductGenericViewSet, basename='products')
 urlpatterns = router.urls
--- backend/products/viewsets.py
+++ backend/products/viewsets.py
@@ -1,10 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 
 from .models import Product
 from .serializers import ProductSerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk' #default
+
+class ProductGenericViewSet(mixins.ListModelMixin,mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'pk' #default
